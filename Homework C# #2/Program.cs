9024007a346e747365ff9ca8a14fd451148bb5e1
Dauren <daurmegatron@gmail.com>
2022-11-28 03:19:05 +0600

    Решения второго ДЗ

---
+++ Homework C# #2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+  if (numberA == 6 || numberA == 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (numberA < 1 || numberA > 7) {
+    Console.WriteLine("это вообще не день недели");
+  }
+  else Console.WriteLine("(этот день не выходной) -> нет");
---
+++ Homework C# #3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+  if (numberA == 6 || numberA == 7) {
+  Console.WriteLine($"{numberA} -> да");
+  }
+  else if (numberA < 1 || numberA > 7) {
+    Console.WriteLine("это вообще не день недели");
+  }
+  else Console.WriteLine($"{numberA} -> нет");
+  
